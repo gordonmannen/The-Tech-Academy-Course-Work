1406450cd023793beb8515d472d8bf5750a97f1a
Gordon Mannen <gordon.mannen@yahoo.com>
2016-04-24 20:37:39 -0700

    Adding File

---
+++ JavaScript/Lynda.com Intro to JavaScript wJoe Chellman/0201.js
@@ -0,0 +1,21 @@
+var x = 32;
+x;
+
+var whereAmI = "Graz, Austria";
+whereAmI;
+
+x = 45;
+x;
+whereAmI = 75;
+whereAmI;
+
+var monster1 = "Grover", monster2 = 'Cookie Monster', monster3 = 'Animal';
+monster1;
+monster2;
+monster3;
+
+var presidentOfUSA = "Obama"; // as of late summer 2012
+presidentOfUSA;
+
+// More info:
+// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Statements/var
