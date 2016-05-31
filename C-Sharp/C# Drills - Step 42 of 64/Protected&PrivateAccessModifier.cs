3bb4e1111525c99fac84b98208c9c77b179d2446
Gordon Mannen <gordon.mannen@yahoo.com>
2016-05-30 21:43:05 -0700

    Added Files

---
+++ C-Sharp/C# Drills - Step 42 of 64/Protected&PrivateAccessModifier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+// Access Modifiers:  public, protected, internal, private, & protected internal.
+
+class A
+{
+    // protected variable
+    protected int _x;
+    private int _y;
+}
+
+class B : A
+{
+    public B()
+    {
+        // Able to access protected int, but not the private int
+        Console.WriteLine(this._x);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        B y = new B();
+    }
+}
---
+++ C-Sharp/C# Drills - Step 42 of 64/PublicAccessModifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+// Access Modifiers:  public, protected, internal, private, & protected internal.
+
+
+// Illustration of public access modifier
+class PublicClassTest
+{
+    string parentString;
+    public void PublicMethodTest()
+    {
+        // definition of PublicMethodTest
+    }
+}
+
+// To access PublicMethodTest
+class Program
+{
+    static void Main(strong[] args)
+    {
+        PublicClassTest objPublicClassTest = new PublicClassTest();
+
+        objPublicClassTest.PublicMethodTest(); // no restrictions, should be able to access
+    }
+}
+
