7d5eecb6d9c00e9b6774ee3fa419dd0361efc191
Gordon Mannen <gordon.mannen@yahoo.com>
2016-06-03 19:51:05 -0700

    Adding Files

---
+++ C-Sharp/C# Drills - Step 42 of 64/AbstractClassVsInterface.cs
@@ -0,0 +1,98 @@
+﻿// Abstract class
+
+// An abstract class is a special type of class that cannot be instantiated.
+// However, it should be inherited by its subclasses.  Can be partially
+// implemented or not at all.  Can have functionality, the methods can be both
+// abstract and concrete.  Can have constructors (this is a major difference
+// between abstract class and interface.
+
+// Abstract class can contain method definitions, fields, and
+// also constructors.
+
+using System;
+
+namespace AbstractClass
+{
+    public abstract class AbstracTest
+    {
+
+        // fields and properties
+
+        protected String account;
+
+        // proerties
+
+        public abstract String ACCOUNT
+        {
+            get;
+            set;
+        }
+
+        // completed methods
+
+        public String Update()
+        {
+            return "Account " + " " + " updated";
+        }
+
+        public String Add()
+        {
+            return "Account " + " " + " added";
+        }
+
+        public String Delete()
+        {
+            return "Account " + " " + " deleted";
+        }
+
+        public String Query()
+        {
+            return "Account " + " " + " located";
+        }
+
+        public abstract String CalculateDailySales();
+
+    }
+}
+
+// ----------------------------------------------------------
+// Interface
+
+// Interface is essentially a contract, it doesn't have any implementation.
+// Can contain only method declaration, cannot contain method definition.
+// It cannot contain any member data.  It can include declaration of events,
+// methods, and properties.  Like abstract class, an interface cannot be
+// instantiated.
+
+// Methods declared should be implemented by the classes that implement the
+// interface.  
+
+using System;
+
+namespace Interface
+{
+    public interface IInterface
+    {
+
+        // cannot have fields, just signature of the properties & methods
+
+        String ACCOUNT
+        {
+            get;
+            set;
+        }
+
+        // cannot have implementation
+
+        String Update();
+
+        String Add();
+
+        String Delete();
+
+        String Query();
+
+        String CalculateDailySales();
+
+    }
+}
---
+++ C-Sharp/C# Drills - Step 42 of 64/Serialize&DeserializeABlob-StreamToText.cs
@@ -0,0 +1,42 @@
+﻿// Serialization of an object - BLOB (Binary Large Object).  The object should be
+// streamed to a text file.  Show also the retrieval of the object from that file and
+// conversion back to its original form.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Test;
+
+namespace BlobToSerialize
+{
+    class Program
+    {
+        FileStream fs = File.Open(BlobToSerialize.img, FileMode.CreateNew, FileAccess.Write);
+    }
+
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8));
+        }
+    }
+}
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Test;
+
+namespace BlobToDeserialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(fs, System.Encoding.img));
+        }
+    }
+}
