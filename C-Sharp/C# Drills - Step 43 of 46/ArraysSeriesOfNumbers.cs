549df32004418ef1d004e794732913c3b832a50f
Gordon Mannen <gordon.mannen@yahoo.com>
2016-06-17 21:49:04 -0700

    Adding File

---
+++ C-Sharp/C# Drills - Step 43 of 46/ArraysSeriesOfNumbers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysPrintingSeriesOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printSeries();
+
+            Console.ReadLine();
+
+
+
+        }
+
+        public static void printSeries()
+
+        {
+
+            int[,] tArr = new int[5, 5];
+
+            int i, j;
+
+
+
+            for (i = 0; i < 5; i++) //assign values to the two-dimensional array
+
+                for (j = 0; j < 5; j++)
+
+                {
+
+                    if (i == 0) tArr[i, j] = j + 1; //fill the first row
+
+                    else if (i > 0 && j == 0)
+
+                        tArr[i, j] = tArr[i - 1, 4] + 1; //fetching the value of the last cell in the previous row
+
+                    else
+
+                        tArr[i, j] = tArr[i, j - 1] + 1; //fill subsequent cells
+
+                }
+
+
+
+            for (i = 0; i < 5; i++)
+            { //print the array
+
+                for (j = 0; j < 5; j++)
+
+                    Console.Write("{0}\t", tArr[i, j]);
+
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
