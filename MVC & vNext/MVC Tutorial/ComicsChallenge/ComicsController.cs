74eb57c80b86ac1492ba02f2c0c469e3914c26a9
Gordon Mannen <gordon.mannen@yahoo.com>
2016-07-23 21:23:00 -0700

    Added Files

---
+++ MVC & vNext/MVC Tutorial/ComicsChallenge/ComicsController.cs
@@ -0,0 +1,26 @@
+﻿using ComicsChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicsChallenge.Controllers
+{
+    public class ComicsController : Controller
+    {
+        // GET: Comics
+        public ActionResult Index()
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            return View(comics);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            var comic = comics.FirstOrDefault(p => p.ComicBookId == id);
+            return View(comic);
+        }
+    }
+}
